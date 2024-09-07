618b142dfaf20dd073c9e08d451e0512fb30f547
Shoon Lei May <shoon@Shoons-MacBook-Air.local>
2024-09-08 01:55:01 +0900

    update

--- seonbae/src/App.js
+++ seonbae/src/App.js
@@ -6,6 +6,8 @@ import UpdateProfile from './Components/UpdateProfile/UpdateProfile.jsx';
 import Homepage from './Components/Homepage/homepage.jsx';
 import LoginSignup from './Components/LoginSignup/LoginSignup.jsx';
 import Info from './Components/Info/Info.jsx'; 
+import InfoConfirmation from './Components/InfoConfirmation/InfoConfirmation.jsx';
+import Edit from './Components/Edit/edit.jsx';
 
 function App() {
   return (
@@ -15,7 +17,8 @@ function App() {
         <Route path="/home-page" element={<Homepage />}/>
         <Route path="/user-info" element={<Info/>}/>
         <Route path="/update-profile" element={<UpdateProfile />} />
-    
+        <Route path="/user-info" element={<InfoConfirmation />} />
+        <Route path="/user-edit" element={<Edit />} />
       </Routes>
     </Router>
   );
