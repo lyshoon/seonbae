81fb80e8e8f88b5a44239ce5f09ff8e839f50d8f
shoon <144576705+lyshoon@users.noreply.github.com>
2024-09-07 21:14:48 +0900

    Update Info.jsxss

--- seonbae/src/Components/Info/Info.jsx
+++ seonbae/src/Components/Info/Info.jsx
@@ -1,36 +1,58 @@
 import React from 'react';
 import './Info.css';
+import { FaFacebook, FaInstagram } from 'react-icons/fa'; 
 
-const Info = () => { 
+const Info = () => {
+  
+  const user = {
+    name: "user",
+    university: "Korea University",
+    major: "Computer Science",
+    nationality: "myanmar",
+    degree: "Bachelor's",
+    facebookProfile: "https://www.facebook.com/shoon.may.1804", 
+    instagramProfile: "https://www.instagram.com/username",
+  };
+
   return (
     <div className="info-container">
       <div className="info-form">
         <div className="form-group">
           <label>Name:</label>
-          <input type="text" value="sss" readOnly />
+          <input type="text" value={user.name} readOnly />
         </div>
         <div className="form-group">
           <label>University:</label>
-          <input type="text" value="Kyung Hee University" readOnly />
+          <input type="text" value={user.university} readOnly />
         </div>
         <div className="form-group">
           <label>Major:</label>
-          <input type="text" value="Software Engineering" readOnly />
+          <input type="text" value={user.major} readOnly />
         </div>
         <div className="form-group">
           <label>Nationality:</label>
-          <input type="text" value="Myanmar" readOnly />
+          <input type="text" value={user.nationality} readOnly />
+        </div>
+        <div className="form-group">
+          <label>Degree:</label>
+          <input type="text" value={user.degree} readOnly />
         </div>
       </div>
 
+      <div className="social-media-icons">
+        <a href={user.facebookProfile} target="_blank" rel="noopener noreferrer">
+          <FaFacebook className="social-icon" />
+        </a>
+        <a href={user.instagramProfile} target="_blank" rel="noopener noreferrer">
+          <FaInstagram className="social-icon" />
+        </a>
+      </div>
+
       <div className="bottom-section">
-        <button className="notify-button">
-          Notify the user to let them know you added them on their social media
-        </button>
         <button className="review-button">Leave a review</button>
       </div>
     </div>
   );
 };
 
-export default Info; 
+export default Info;
