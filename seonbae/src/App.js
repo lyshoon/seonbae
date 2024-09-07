d145d99f7eaecdb42c70073165a905f4ff2aa347
suleileinaing <yumi.sulei@gmail.com>
2024-09-07 15:27:27 +0900

    hi

--- seonbae/src/App.js
+++ seonbae/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import logo from './logo192.png';
 import './App.css';
+import SignUp from './SignUp';
+
+import {ChakraProvider, Box} from "@chakra-ui/react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider> 
+      <Router>
+        <Routes> 
+          <Route path="/sign-up" element = {<SignUp />} />
+        </Routes>
+      </Router>
+    </ChakraProvider>
   );
 }
 
