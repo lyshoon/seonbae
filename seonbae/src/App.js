ac799f4f2561c3dc861dc4de2078fd1f11048a6a
Shoon Lei May <shoon@Shoons-MacBook-Air.local>
2024-09-07 17:43:54 +0900

    update

--- seonbae/src/App.js
+++ seonbae/src/App.js
@@ -1,9 +1,10 @@
-
+import React, { useState } from 'react';
 import './App.css';
 import Homepage from './Components/Homepage/homepage.jsx';
 import LoginSignup from './Components/LoginSignup/LoginSignup.jsx'
 
 function App() {
+  
   return (
     <div>
       <LoginSignup/>
