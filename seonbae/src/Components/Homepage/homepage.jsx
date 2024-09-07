a946e614650ecab7c61557347f6f6b988a5be22d
shoon <144576705+lyshoon@users.noreply.github.com>
2024-09-07 17:48:42 +0900

    Update homepage.jsx

--- seonbae/src/Components/Homepage/homepage.jsx
+++ seonbae/src/Components/Homepage/homepage.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './homepage.css'; 
 
 const Homepage = () => {
+
+  const [users, setUsers] = useState([
+    {
+      name: 'Byun Baekhyun',
+      age: 21,
+      country: 'Korea',
+      department: 'Computer Science and Engineering',
+      university: 'Korea University',
+    },
+    {
+      name: 'Shoon Lei May',
+      age: 21,
+      country: 'Myanmar',
+      department: 'Computer Science and Engineering',
+      university: 'Korea University',
+    },
+    
+  ]);
+
   return (
     <div className="homepage-container">
-      {/* Top Bar */}
+      
       <div className="top-bar">
         <h1 className="logo">SeonBae</h1>
         <div className="user-controls">
@@ -14,18 +33,21 @@ const Homepage = () => {
       </div>
 
       <div className="search-container">
-        <label htmlFor="university-search">Enter Your University :</label>
-        <input type="text" id="university-search" placeholder="search"/>
+        <label htmlFor="university-search">Enter Your University or Country or Major :</label>
+        <input type="text" id="university-search" placeholder="search" />
       </div>
 
       <div className="recommendations">
         <h2>Here are some people from your University</h2>
-        <div className="profile-card">
-          <p><strong>Su Lei Lei Naing (23)</strong> MYANMAR</p>
-          <p>Software Engineering Department</p>
-          <p>Kyung Hee University</p>
-        </div>
-        {/* Add more profile cards here as needed */}
+        
+        
+        {users.map((user, index) => (
+          <div key={index} className="profile-card">
+            <p><strong>{user.name} ({user.age})</strong> {user.country}</p>
+            <p>{user.department}</p>
+            <p>{user.university}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
